9f4ccd219cf2c5c19df48ea90d14afad0c0d93c0
SteShi <ste-shi@mail.ru>
2019-08-21 15:40:38 +0300

    new file:   src/index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+
+console.log('Welcome to the Brain Games!');
+export const privet = readlineSync.question('Kak zvat? ');
+console.log(`Hello ${privet}`);
+
