db3bc84b5207e51b8a241b86d66ef6cc6ccb571d
SteShi <ste-shi@mail.ru>
2019-08-22 15:51:13 +0300

    modified:   .eslintrc.yml 	modified:   package-lock.json 	modified:   package.json 	modified:   src/bin/brain-games.js 	modified:   src/index.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {name} from '..';
+import name from '..';
 
 console.log('Welcome to Brain Games!!');
 console.log(`Hello ${name('what is you name?')}!!`);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-export const name = str => readlineSync.question(str);
+
+export default (str) => readlineSync.question(str);
