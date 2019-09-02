c96801eaf7084c391295d519f2cb83479cbd7f0b
SteShi <ste-shi@mail.ru>
2019-09-02 21:07:20 +0300

    dvig.js console.log-->constants (Eslint satisfied)

--- src/dvig.js
+++ src/dvig.js
@@ -1,21 +1,24 @@
 import readlineSync from 'readline-sync';
 import { head, tail, isEmpty } from '@hexlet/pairs-data';
 
+
 const check = (needList, name, quList) => {
   if (isEmpty(quList)) {
-    console.log(`Congratulation, ${name}`);
-  } else {
-    console.log(`Question: ${head(quList)}`);
-    const get = readlineSync.question('Your answer:');
-    const need = head(needList);
-    if (String(get) !== String(need)) {
-      console.log(`${get} is wrong answer^-^ correct answer was ${need}`);
-      console.log(`Lets try again, ${name}`);
-    } else {
-      console.log('correct!!!!!!');
-      return check(tail(needList), name, tail(quList));
-    }
+    const console1 = console.log(`Congratulation, ${name}`);
+    return console1;
   }
+  console.log(`Question: ${head(quList)}`);
+  const get = readlineSync.question('Your answer:');
+  const need = head(needList);
+  const console2 = () => {
+    console.log(`${get} is wrong answer^-^ correct answer was ${need}`);
+    console.log(`Lets try again, ${name}`);
+  };
+  if (String(get) !== String(need)) {
+    return console2;
+  }
+  console.log('correct!!!!!!');
+  return check(tail(needList), name, tail(quList));
 };
 
 export default check;
