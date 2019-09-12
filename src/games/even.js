912a6df271a30cf96c6a90ec92a4e8248bf774ad
SteShi <ste-shi@mail.ru>
2019-09-12 20:03:38 +0300

    isEven update

--- src/games/even.js
+++ src/games/even.js
@@ -3,11 +3,11 @@ import check from '../engine';
 import randomInRange from '../lib/utils';
 
 const description = 'Answer "yes" if number even otherwise answer "no".';
-const isEven = (num) => (((num % 2) === 0) ? 'yes' : 'no');
+const isEven = (num) => ((num % 2) === 0);
 
 const generator = () => {
   const question = randomInRange(1, 20);
-  const rightAnswer = isEven(question);
+  const rightAnswer = (isEven(question) === true) ? 'yes' : 'no';
   return cons(question, rightAnswer);
 };
 
